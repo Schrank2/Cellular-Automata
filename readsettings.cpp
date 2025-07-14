b3e92e0ad88ee1ed3044cc5c07d8a18a35d67d9a
Schrank <schrank1311@gmail.com>
2025-07-14 11:33:19 +0200

    Weird div by zero error

--- readsettings.cpp
+++ readsettings.cpp
@@ -5,12 +5,12 @@
 #include "functions.h"
 #include "defs.h"
 using namespace std;
-int GameScale = 10; // The Dimensions of each tile in pixels
-float mapDensity = 0.5; // The density of Cells in the beginning in 0.0 -> 1.0
-int ScreenWidth = 1600; // The width of the game window in pixels
-int ScreenHeight = 900; // The height of the game window in pixels
-float ThreadCountUsage = 0.5; // The percentage of CPU threads to use for the game
-int TickTime = 150; // Time between game ticks in milliseconds
+int GameScale; // The Dimensions of each tile in pixels
+float mapDensity; // The density of Cells in the beginning in 0.0 -> 1.0
+int ScreenWidth; // The width of the game window in pixels
+int ScreenHeight; // The height of the game window in pixels
+float ThreadCountUsage; // The percentage of CPU threads to use for the game
+int TickTime; // Time between game ticks in milliseconds
 // https://www.w3schools.com/cpp/cpp_files.asp
 
 int readSettings() {
@@ -19,23 +19,45 @@ int readSettings() {
 	// Read from the text file
 	ifstream MyReadFile("options.txt");
 	vector<string> lines;
-	vector<string> linesDivided;
 	vector<vector<string>> settings;
 	string line;
 	while (getline(MyReadFile, line)) {
 		lines.push_back(line);
 	}
 	for (int i = 0; i < lines.size(); i++) {
-		std::istringstream iss(lines[i]);  // create a string stream from the line
-		std::string key;
-		while (getline(iss, key, '=')) {
-			linesDivided.push_back(key);
+		istringstream iss(lines[i]);  // create a string stream from the line
+		string key;
+		size_t pos = lines[i].find_first_not_of(" \t");
+		if (pos != string::npos) {
+			if (lines[i][pos] != '#') {
+				settings.push_back(vector<string>());
+				while (getline(iss, key, '=')) {
+					settings.back().push_back(key);
+				}
+			}
 		}
 	}
 	// Close the file
 	MyReadFile.close();
-	for (const auto& l : linesDivided) {
-		cout << l << endl;
+	for (const auto& l : settings) {
+		cout << l[0] << "///" << l[1] << endl;
+	}
+	for (int i = 0; i < settings.size(); i++) {
+		string key = settings[i][0];
+		string value = settings[i][1];
+		if (key== "GameScale") {
+			GameScale = stoi(value);
+		} else if (key == "mapDensity") {
+			mapDensity = stof(value);
+		} else if (key == "ScreenWidth") {
+			ScreenWidth = stoi(value);
+		} else if (key == "ScreenHeight") {
+			ScreenHeight = stoi(value);
+		} else if (key == "ThreadCountUsage") {
+			ThreadCountUsage = stof(value);
+		} else if (key == "TickTime") {
+			TickTime = stoi(settings[i][1]);
+		}
 	}
 	return 1;
 }
